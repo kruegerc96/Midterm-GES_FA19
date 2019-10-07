87a43a0e459cc3864045b8f9ef1c8a0a55c2f062
Cole Krueger <kruegerc96@gmail.com>
2019-10-07 15:09:22 -0500

    added movement
    assdhjkghjk

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,35 +5,39 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     // movement variables
-    [SerializeField] private float speed = 10f;
-    [SerializeField] private float sprintMultiplier = 1.5f;
-    private float horizontalInput;
-    private float forwardInput;
+    [SerializeField] private float speed;
+    [SerializeField] private float sprintMultiplier;
+    private bool isSprinting;
 
     // aim variables
-    [SerializeField] private float horizontalMouseSensitivity = 0.75f;
-    [SerializeField] private float verticalMouseSensitivity = 0.75f;
-
+    [SerializeField] private float horizontalMouseSensitivity;
+    [SerializeField] private float verticalMouseSensitivity;
 
     void Start()
     {
-        
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
     void Update()
     {
-        // move forward
-        horizontalInput = Input.GetAxis("Horizontal");
-        forwardInput = Input.GetAxis("Vertical");
+        // movement
+        float translation = Input.GetAxis("Vertical") * speed;
+        float straffe = Input.GetAxis("Horizontal") * speed;
+        translation *= Time.deltaTime;
+        straffe *= Time.deltaTime;
 
-        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        transform.Translate(straffe, 0, translation);
 
-        // mouse look
-        float deltaMouseHorizontal = Input.GetAxis("Mouse X") * horizontalMouseSensitivity;
-        float deltaMouseVertical = Input.GetAxis("Mouse Y") * verticalMouseSensitivity;
-        float newCameraRotY = transform.eulerAngles.y + deltaMouseHorizontal;
-        float newCameraRotX = transform.eulerAngles.x - deltaMouseVertical;
+        // sprint
+        if (Input.GetKeyDown("LeftShift"))
+        {
+            speed = speed * sprintMultiplier;
+        }
 
-        transform.eulerAngles = new Vector3(newCameraRotX, newCameraRotY, 0);
+        // unlock cursor if needed
+        if (Input.GetKeyDown("escape"))
+        {
+            Cursor.lockState = CursorLockMode.None;
+        }
     }
 }
---
+++ Assets/Scripts/mouseLook.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mouseLook : MonoBehaviour
+{
+    // aim variables
+    [SerializeField] private float horizontalMouseSensitivity = 0.75f;
+    [SerializeField] private float verticalMouseSensitivity = 0.75f;
+
+    void Update()
+    {
+        // adjust camera
+        float deltaMouseHorizontal = Input.GetAxis("Mouse X") * horizontalMouseSensitivity;
+        float deltaMouseVertical = Input.GetAxis("Mouse Y") * verticalMouseSensitivity;
+        float newCameraRotY = transform.eulerAngles.y + deltaMouseHorizontal;
+        float newCameraRotX = transform.eulerAngles.x - deltaMouseVertical;
+
+        transform.eulerAngles = new Vector3(newCameraRotX, newCameraRotY, 0);
+    }
+}

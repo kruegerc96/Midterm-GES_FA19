d8d654aba1b8e9ead2da82f3b3d49a58f0ee3ebd
kruegerc96 <49258382+kruegerc96@users.noreply.github.com>
2019-11-24 20:55:12 -0600

    BulletSpawnLocationWorks
    dafgdhfjhgjkjdgfh

--- Assets/Scripts/bulletShoot.cs
+++ Assets/Scripts/bulletShoot.cs
@@ -4,21 +4,26 @@ using UnityEngine;
 
 public class bulletShoot : MonoBehaviour
 {
-    [SerializeField] GameObject bullet;
-    [SerializeField] float shootForce = 10f;
-    [SerializeField] float shotsPerSecond = 10;
+    [SerializeField] Rigidbody bullet;
+    [SerializeField] float bulletSpeed;
+    [SerializeField] float fireRate;
+    [SerializeField] GameObject bulletSpawn;
+    bool canShoot = true;
 
+    // Update is called once per frame
     void Update()
     {
-        while (Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButtonDown(0))
         {
-            GameObject bulletPrefab = Instantiate(bullet, transform.position, Quaternion.identity);
-            Rigidbody rb = bullet.GetComponent<Rigidbody>();
+            Rigidbody instantiatedBullet = Instantiate(bullet, bulletSpawn.transform.position, bulletSpawn.transform.rotation);
+            instantiatedBullet.velocity = transform.TransformDirection(new Vector3(0, 0, bulletSpeed));
 
-            // A quaternion multiplied by a Vector applies that rotation to the Vector.
-            Vector3 direction = Quaternion.Euler(0, 0, 15) * transform.forward;
+            Invoke("EnableShooting", 1 / fireRate);
+        }
 
-            rb.AddForce(direction * shootForce, ForceMode.Impulse);
+        void EnableShooting()
+        {
+            canShoot = true;
         }
     }
 }
---
+++ Assets/Scripts/mouseLook.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mouseLook : MonoBehaviour
+{
+    public float lookSpeed = 3;
+    private Vector2 rotation = Vector2.zero;
+
+    public void Look() // Look rotation (UP down is Camera) (Left right is Transform rotation)
+    {
+        rotation.y += Input.GetAxis("Mouse X");
+        rotation.x += -Input.GetAxis("Mouse Y");
+
+        rotation.x = Mathf.Clamp(rotation.x, -15f, 15f);
+
+        transform.eulerAngles = new Vector2(0, rotation.y) * lookSpeed;
+
+        Camera.main.transform.localRotation = Quaternion.Euler(rotation.x * lookSpeed, 0, 0);
+    }
+}
